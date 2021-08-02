c038ee911a62b055c566e74f91fc4cfc29d04a41
Yulandia <tyakina88@gmail.com>
2021-08-02 15:25:01 -0500

    (Added 5.3.4.py homework)

---
+++ 5.3.4.py
@@ -0,0 +1,6 @@
+def greeting():
+    user_input = input("Enter your first name: ")
+    user_input1 = input("Enter your lats name: ")
+    print("Hello, " + (user_input) + " " + (user_input1))
+
+greeting()
